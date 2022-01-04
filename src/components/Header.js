fa4cd229d845e6d3be3c1ba416135ef42e22c102
Alish Palasara <palasaraalis@gmail.com>
2022-01-04 17:42:43 +0530

    feat: Initial commit

---
+++ src/components/Header.js
@@ -0,0 +1,19 @@
+import PropTypes from 'prop-types';
+
+const Header = ({ title }) => {
+  return (
+    <div className="container">
+      <h1>{title}</h1>
+    </div>
+  );
+};
+
+Header.defaultProps = {
+  title: 'My App',
+};
+
+Header.propTypes = {
+  title: PropTypes.string.isRequired,
+};
+
+export default Header;
